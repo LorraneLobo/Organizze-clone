1bca37e495c2e354506c934cf20ecbbc53d2fd6a
Lorrane Alves <Lorrane.dev@gmail.com>
2021-11-24 00:44:22 -0300

    Adicionado slides

--- app/src/main/java/com/example/organizze/MainActivity.java
+++ app/src/main/java/com/example/organizze/MainActivity.java
@@ -5,12 +5,61 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.heinrichreimersoftware.materialintro.app.IntroActivity;
+import com.heinrichreimersoftware.materialintro.slide.FragmentSlide;
+import com.heinrichreimersoftware.materialintro.slide.SimpleSlide;
 
 public class MainActivity extends IntroActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        setButtonBackVisible(false);
+        setButtonNextVisible(false);
+
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_1)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_2)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_3)
+                .build()
+        );
+        addSlide(new FragmentSlide.Builder()
+                .background(android.R.color.white)
+                .fragment(R.layout.intro_4)
+                .canGoForward(false)
+                .build()
+        );
+//
+//        addSlide(new SimpleSlide.Builder()
+//                .title("Titulo")
+//                .description("Descricao")
+//                .image(R.drawable.um)
+//                .background(android.R.color.holo_orange_light)
+//                .build()
+//        );
+//        addSlide(new SimpleSlide.Builder()
+//                .title("Titulo2")
+//                .description("Descricao2")
+//                .image(R.drawable.dois)
+//                .background(android.R.color.holo_orange_light)
+//                .build()
+//        );
+//        addSlide(new SimpleSlide.Builder()
+//                .title("Titulo3")
+//                .description("Descricao3")
+//                .image(R.drawable.tres)
+//                .background(android.R.color.holo_orange_light)
+//                .build()
+//        );
     }
 }
